10a95a07cefff95fbd5bec65c364ab4ada1884e0
linitachi <linitachi@gmail.com>
2018-05-10 21:10:21 +0800

    完成爬商品步驟 但只有一頁面

--- crawler/noroof.py
+++ crawler/noroof.py
@@ -6,31 +6,25 @@ from datetime import datetime
 from selenium import webdriver
 import sys
 data = []
-# quote_page = 'https://www.google.com.tw/search?q=%E4%BD%A0%E5%A5%BD&oq=%E4%BD%A0%E5%A5%BD&aqs=chrome..69i57j0l5.12793j0j8&sourceid=chrome&ie=UTF-8'
 # chromedriver.exe執行檔所存在的路徑
 chrome_path = "C:\selenium_driver_chrome\chromedriver.exe"
 driver = webdriver.Chrome(chrome_path)
 
-driver.get("https://find.ruten.com.tw/c/002100010003")
+driver.get("https://find.ruten.com.tw/c/002100010003?sort=new%2Fdc")
 html = driver.page_source
-driver.close()
+# driver.close()
 soup = BeautifulSoup(html, 'html.parser')
 name = []
 price = []
-for name_box in soup.find_all('h5'):
+
+for name_box in soup.find_all('h5', 'prod_name'):  # 找出商品名
     name.append(name_box.text)
-for price_box in soup.find_all('span'):
-    price.append(price_box.text)
+
+for price_box in soup.find_all('span', attrs={'class': 'price'}):  # 找出價格
+    if(price_box.text != ''):
+        price.append(price_box.text)
+
+# print(len(price), len(name)) 因為有廣告所以價格比商品名多
+
 for i in range(len(name)):
-    print(name[i], price[i])
-'''req = Request('',
-              headers={'User-Agent': 'Mozilla/5.0'})
-page = urlopen(req).read()
-# page = urlopen(quote_page)
-soup = BeautifulSoup(page, 'html.parser')
-data = []
-# for name_box in soup.find('h3', attrs={'class': 'r'}).parent.find_all('h3'):
-for name_box in soup.find_all('h3'):
-    print(name_box.text)
-'''
-sys.exit()
+    print(name[i], '價格為:', price[i])
