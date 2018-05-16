1e8508b4a9205059d9304e741018d5d1c382363e
linitachi <linitachi@gmail.com>
2018-05-16 17:14:57 +0800

    新增商品上架時間

--- crawler/noroof.py
+++ crawler/noroof.py
@@ -25,20 +25,30 @@ def current_page_info(soup):
     return name, price,SourcePage
 
 
-def WriteInExcel(filename, name, price,SourcePage):
+def WriteInExcel(filename, name, price,SourcePage,Addedtime):
     with open(filename, 'a',newline='') as csv_file: #寫入不會有空行newline=''
         writer = csv.writer(csv_file)
         for i in range(len(name)):
-            #name[i]=name[i].encode("utf8")
-            #name[i]=name[i].decode("cp950", "ignore")
             try:
-                writer.writerow([name[i], price[i],SourcePage[i]])
+                writer.writerow([name[i], price[i],SourcePage[i],Addedtime[i]])
             except Exception: #因為無法解決編碼問題，因此把有特殊字元的狀況 丟出例外。
                 name[i]=name[i].encode("utf8")
                 name[i]=name[i].decode("cp950", "ignore")
-                writer.writerow([name[i], price[i],SourcePage[i]])
+                Addedtime[i]=Addedtime[i].encode("utf8")
+                Addedtime[i]=Addedtime[i].decode("cp950", "ignore")
+                writer.writerow([name[i], price[i],SourcePage[i],Addedtime[i]])
     return 1
 
+def Added_time(SourcePage):#SourcePage:list
+    time=[]
+    for j in range(len(SourcePage)):
+        driver.get(SourcePage[j])
+         # 讀取當頁面的html
+        html = driver.page_source
+        # 分析當前頁面的資訊
+        soup = BeautifulSoup(html, 'html.parser')
+        time.append(soup.find('li','upload-time').text)
+    return time
 
 # chromedriver.exe執行檔所存在的路徑
 chrome_path = "C:\selenium_driver_chrome\chromedriver.exe"
@@ -49,6 +59,7 @@ url = 'https://find.ruten.com.tw/c/002100010003?sort=new%2Fdc'
 
 times = 1  # 跑幾個頁面
 page = 2  # 代表第幾頁
+ewww=[]
 for i in range(times):
     # 開啟網頁
     driver.get(url)
@@ -57,7 +68,7 @@ for i in range(times):
     # 分析當前頁面的資訊
     soup = BeautifulSoup(html, 'html.parser')
     name, price,SourcePage = current_page_info(soup)
-    WriteInExcel('test.csv', name, price,SourcePage)
+    WriteInExcel('test.csv', name, price,SourcePage,Added_time(SourcePage))
     # 找出下一頁的網址
     url = 'https://find.ruten.com.tw/c/002100010003?p=' + \
         str(page)+'&sort=new%2Fdc'
