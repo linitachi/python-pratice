d9383a68765d265590f4cc89ec473f09e6585b7c
linitachi <linitachi@gmail.com>
2018-05-25 14:44:48 +0800

    完成
    資料可以切開，但名字有"，"的會出錯

--- crawler/datahandle.py
+++ crawler/datahandle.py
@@ -8,17 +8,21 @@ import sys
 import re
 
 def WriteInExcel(filename, data,cellphone):
-    with open(filename, 'a',newline='') as csv_file: #寫入不會有空行newline=''
-        writer = csv.writer(csv_file)
+    with open(filename, 'w',newline='') as csv_file: #寫入不會有空行newline=''
         for i in range(len(data)):
+            writer = csv.writer(csv_file,delimiter=' ')
             writer.writerow(cellphone[i])
             for j in range(len(data[i])):
+                writer = csv.writer(csv_file)
                 try:
-                    writer.writerow([data[i][j]])
+                    gg=data[i][j].split(',')
+                    if(len(gg)==4):
+                        gg[1]+=gg.pop(2)
+                    writer.writerow(gg)
                 except Exception: #因為無法解決編碼問題，因此把有特殊字元的狀況 丟出例外。
                     data[i][j]=data[i][j].encode("utf8")
                     data[i][j]=data[i][j].decode("cp950", "ignore")
-                    writer.writerow([data[i][j]])
+                    writer.writerow(gg)
     return 1
 newdata=[]
 i=0 #控制excel行數
@@ -26,7 +30,6 @@ j=0 #控制手機型號
 f=open("test.csv",'r')
 data=f.readlines()
 cellphone=["xzp",'xz1','xz','z5','z5p','xz2','other']
-e=[1,2,4]
 for k in range (len(cellphone)):
     newdata.append([])
 while(1):
@@ -40,8 +43,7 @@ while(1):
             newdata[j].append(data[i])
             i+=1
             j=0
-    print(i)
     if(i==len(data)):
         break
-print(newdata)
+
 WriteInExcel('final.csv',newdata,cellphone)
